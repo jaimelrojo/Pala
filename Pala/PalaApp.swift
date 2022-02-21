7ec02f4866957dab79286eb62e3158b8de44b2d2
jaimelrojo <jaime.casta@icloud.com>
2022-02-21 05:06:17 -0600

    Initial Commit

---
+++ Pala/PalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalaApp.swift
+//  Pala
+//
+//  Created by Jaime Castañeda on 21/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
